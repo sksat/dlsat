205cd47f095245aa6fffcf036bef0180cc9a0d52
sksat <sksat@users.noreply.github.com>
2021-08-03 07:51:15 +0000

    format: prettier

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,30 +1,30 @@
-import React from 'react';
+import React from "react";
 import Head from "next/head";
-import './App.css';
+import "./App.css";
 
 import { VideoList } from "./components/VideoList";
 
 const TITLE = "動画ダウンロード太郎";
 
 function HomePage() {
-	return (
-	<>
-		<Head>
-			<title>{TITLE}</title>
-		</Head>
-		<h1>{TITLE}</h1>
-		<br/>
-		<form action="/api/download" method="post">
-			<h3>URLかなにか</h3>
-			<input type="text" name="param" />
+  return (
+    <>
+      <Head>
+        <title>{TITLE}</title>
+      </Head>
+      <h1>{TITLE}</h1>
+      <br />
+      <form action="/api/download" method="post">
+        <h3>URLかなにか</h3>
+        <input type="text" name="param" />
 
-			<br/>
-			<input type="submit" value="ダウンロード" />
-		</form>
+        <br />
+        <input type="submit" value="ダウンロード" />
+      </form>
 
-		<VideoList />
-	</>
-	);
+      <VideoList />
+    </>
+  );
 }
 
 export default HomePage;
